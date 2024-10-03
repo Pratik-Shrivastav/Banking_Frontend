37b72dba1707906efe3afb7639f593dcea159218
Pratik Shrivastav <Pratik.Shrivastav@asl.mum.sst>
2024-10-03 20:23:05 +0530

    Google Captcha

--- src/app/modules/login/components/login-page/login-page.component.ts
+++ src/app/modules/login/components/login-page/login-page.component.ts
@@ -1,25 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { LoginService } from '../../service/login.service';
 import { Login } from '../../../../models/login';
 import { LoginResponse } from '../../../../models/login-response';
 import { Router } from '@angular/router';
+import { NgxCaptchaModule } from 'ngx-captcha';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css'
+  styleUrl: './login-page.component.css',
 })
 export class LoginPageComponent {
   loginForm: FormGroup;
-  //siteKey = '6Lf151QqAAAAACVNLhJNZaJPUl1Z7aG4oVh9MP7W';
+  recaptchaResponse: string | undefined;
 
   constructor(private fb: FormBuilder, private loginService: LoginService, private router: Router) {
     this.loginForm = this.fb.group({
       userName: ['', Validators.required],
       password: ['', Validators.required],
-      // recaptcha: ['', Validators.required]
+      recaptcha: ['', Validators.required]
     });
+  }
+
+  handleSuccess(captchaResponse: string) {
+    this.recaptchaResponse = captchaResponse; // Store the captcha response
+    console.log(`Captcha resolved with response: ${this.recaptchaResponse}`);
+    this.loginForm.patchValue({ recaptcha: this.recaptchaResponse }); // Update the form control
+  }
+
+  resolved(captchaResponse: any) {
+    this.recaptchaResponse = captchaResponse;
+    console.log(`Resolved captcha with response: ${this.recaptchaResponse}`);
   }
 
   onSubmit() {
@@ -33,7 +45,8 @@ export class LoginPageComponent {
             localStorage.setItem("Token", response.token);
             this.router.navigate(['SuperAdmin']);
           }
-          else {
+          else 
+          {
             if (response.success && response.status == "Success") {
               console.log('Login successful!', response);
               localStorage.setItem("Token", response.token);
