4c9a24f60fd28ce403e1b70abd59bb4029326b77
Pratik Shrivastav <Pratik.Shrivastav@asl.mum.sst>
2024-10-04 21:50:13 +0530

    search

--- src/app/modules/super-admin/components/clients/clients.component.ts
+++ src/app/modules/super-admin/components/clients/clients.component.ts
@@ -1,39 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SuperAdminService } from '../../service/super-admin.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
-  styleUrl: './clients.component.css'
+  styleUrls: ['./clients.component.css']
 })
-export class ClientsComponent {
+export class ClientsComponent implements OnInit {
   searchForm!: FormGroup;
+  clientsAll: any; // Hold the clients found by search
   clients: any[] = []; // Array to hold the list of clients
   loading: boolean = true; // Flag to show loading state
   currentPage = 1; // Track the current page
   totalClients = 0; // Total number of clients from the backend
   pageSize = 8; // Number of clients per page
 
-  constructor(private fb: FormBuilder,private superAdminService: SuperAdminService, private router:Router)
-   {
+  constructor(private fb: FormBuilder, private superAdminService: SuperAdminService, private router: Router) {
     this.searchForm = this.fb.group({
-      searchTerm: [''] // Create a form control for search term
+      searchTerm: [''] 
     });
-    }
+  }
 
   ngOnInit(): void {
     this.loadClients(); // Fetch clients when the component initializes
+    this.searchForm.get('searchTerm')?.valueChanges
+      .pipe(debounceTime(500)) // Wait for 300ms pause in events
+      .subscribe(() => this.onSearch()); // Trigger search on value changes
   }
 
   loadClients(): void {
     this.superAdminService.getpaginationClients(this.currentPage, this.pageSize).subscribe({
       next: (response) => {
-        console.log(response);
-        console.log(response.clients);
-        
-        this.clients = response.clients; // Store the clients returned from the backend
+        this.clientsAll = response.clients;
+        this.clients = this.clientsAll // Store the clients returned from the backend
         this.totalClients = response.totalCount; // Store the total count of clients
         this.loading = false; // Hide loading indicator
       },
@@ -49,15 +51,18 @@ export class ClientsComponent {
     this.loadClients(); // Fetch clients for the new page
   }
 
-  onCardClick(id:number) {
-    this.router.navigate(['SuperAdmin/ViewClient', id])
-    }
-
-  searchClient(){
-    this.superAdminService.getClientByName(this.searchForm.value.searchTerm).subscribe((clientsByname)=>
-      {
-        this.clients = clientsByname;
-      })
+  onCardClick(id: number) {
+    this.router.navigate(['SuperAdmin/ViewClient', id]);
   }
 
+  onSearch(): void {
+    const searchTerm = this.searchForm.get('searchTerm')?.value;
+    if (searchTerm) {
+      this.superAdminService.getClientByName(searchTerm).subscribe((clientsByName) => {
+        this.clients = clientsByName;
+      });
+    } else {
+      this.clients = this.clientsAll;
+    }
+  }
 }
